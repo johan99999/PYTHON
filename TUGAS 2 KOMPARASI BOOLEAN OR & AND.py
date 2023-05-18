1cfcf7f74e6d700b3c3f4fcdfe5cbfe17f5cd524
Han Lie <133509785+johan99999@users.noreply.github.com>
2023-05-18 06:02:19 -0700

    Add files via upload
    SEBUAH PROGRAM KECIL UNTUK PENGUJIAN FUNGSI BOOLEAN PADA PYTHON

---
+++ TUGAS 2 KOMPARASI BOOLEAN OR & AND.py
@@ -0,0 +1,28 @@
+print(30*"=")
+
+inputuser = float(input("masukan input :"))
+a = inputuser > 0, inputuser < 5
+print(a)
+inputuser = float(input("masukan input :"))
+b = inputuser > 8, inputuser < 11
+print(b)
+
+c = a or b
+
+print("komparasi antar input ialah",c)
+
+print(30*"=")
+inputuser = float(input("masukan input :"))
+a1 = inputuser < 0, inputuser > 5
+print(a1)
+inputuser = float(input("masukan input :"))
+b1 = inputuser < 8, inputuser > 11
+print(b1)
+
+c1 = a1 and b1
+
+print("komparasi antar input ialah",c112)
+
+print(30*"=")
+
+
