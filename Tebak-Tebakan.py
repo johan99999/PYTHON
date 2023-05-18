38ec4a20062e2caa45c627a47e5a1d7f76db5700
Han Lie <133509785+johan99999@users.noreply.github.com>
2023-05-18 07:12:00 -0700

    Add files via upload
    Script Tebak tebakan berujung bug awaokaowkaowkoakwoakw :v

---
+++ Tebak-Tebakan.py
@@ -0,0 +1,8 @@
+print(30*"-")
+
+print("Pintu? Pintu apa yang gak bisa didorong? : ")
+jawab = input("jawab : ")
+if jawab == ("geser", "Geser", "GESER"):
+    print("awoakwoakwoakwokawok yang ada tulisan", "GESER")
+if jawab != ("geser", "Geser", "GESER"):
+    print("salah goblok")
