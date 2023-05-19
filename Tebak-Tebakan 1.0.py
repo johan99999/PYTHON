7a364c7e894d87948ffa0b5144a9914a39499e35
Han Lie <133509785+johan99999@users.noreply.github.com>
2023-05-19 07:05:24 -0700

    Add files via upload
    update dari "Tebak-tebakan.py"
    bug fixed "salah goblok"
    akhirnya gw mengerti cara if else

---
+++ Tebak-Tebakan 1.0.py
@@ -0,0 +1,9 @@
+print(35*"#")
+
+print("Kota? Kota apa yang banyak bapak-bapaknya?")
+jawab = input("Jawab : ")
+
+if jawab != "purwodaddy" :
+    print("salah goblok")
+else :
+    print("Awaokwaowkaokw yahaha purwodaddy (*emot batu)")
