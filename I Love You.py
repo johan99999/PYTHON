3f4d2b2c777ae0f994c85d37408f90d7d2f1c779
Han Lie <133509785+johan99999@users.noreply.github.com>
2023-05-24 21:31:05 +0700

    Add files via upload
    cara ku mengekspresikan perasaan ku untuknya

---
+++ I Love You.py
@@ -0,0 +1,29 @@
+option = input("Do you Love me ? : ")
+yes = True or ("yes")
+no = False or ("no")
+#for i in option :
+    #print("I Love You")
+#while option == "yes" :
+    #print("I Love You")
+#if option == "no" :
+    #print("Good Bye")
+while option == "no" :
+    print("Good Bye")
+    break
+while option == "yes" :
+    print("I Love You")
+
+inputan = input("did you love me? (y/n) : ")
+if inputan == "no":
+    print("Good Bye")
+    quit()
+
+for i in range (100) :
+
+    print("I Love You")
+
+
+
+
+
+
